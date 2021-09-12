aea881fcbff7d3a0d76ab3095fd634448cb4d107
unknown <05vp94@mail.ru>
2021-09-12 22:07:00 +0300

    first page

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,24 @@
 $(function(){
-    
+    $('.header__btn').click(function(event){
+        $('.rightMenu').toggleClass("active");
+    });
+    $('.rightMenu__btn').click(function(event){
+        $('.rightMenu').removeClass("active");
+    });
+
+
+
+
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true
+    })
+
+    var mixer = mixitup('.gallary__inner', {
+        load: {
+            filter: '.living'
+        }
+    });
 })
